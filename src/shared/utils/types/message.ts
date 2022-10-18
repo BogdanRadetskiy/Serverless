ddd3c5680ded200e62b584ecca82180ea67a5963
BogdanRadetskiy <bogdan.Radetskyi@fivesysdev.com>
2022-10-18 15:14:31 +0300

    fix mesage error

---
+++ src/shared/utils/types/message.ts
@@ -0,0 +1,52 @@
+enum StatusCode {
+  success = 200,
+}
+
+class Result {
+  private readonly statusCode: number;
+  private readonly code: number;
+  private readonly message: string;
+  private readonly data?: any;
+
+  constructor(statusCode: number, code: number, message: string, data?: any) {
+    this.statusCode = statusCode;
+    this.code = code;
+    this.message = message;
+    this.data = data;
+  }
+
+  bodyToString() {
+    return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+      },
+      statusCode: this.statusCode,
+      body: JSON.stringify({
+        code: this.code,
+        message: this.message,
+        data: this.data,
+      }),
+    };
+  }
+}
+
+export class MessageUtil {
+  static success(data: object): any {
+    const result = new Result(StatusCode.success, 0, 'success', data);
+
+    return result.bodyToString();
+  }
+
+  static forward(event, data): any {
+    event.body.statePack = event.body.statePack ?? {};
+    event.body.statePack = Object.assign(event.body.statePack, data);
+    return event;
+  }
+
+  static error(code: number = 1000, message: string) {
+    const result = new Result(code, code, message);
+
+    return result.bodyToString();
+  }
+}
